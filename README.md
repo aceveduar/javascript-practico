b04bee1a31001f946d57a1f38400118f72859674
Eduardo <eduardo@MacBook-Pro-de-Eduardo.local>
2022-05-29 19:46:58 -0500

    En este ejercicio practicamos el uso de operaciones aritméticas, creación y asignación de variables, ademas de poder concatenar los valores con cadenas de texto.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# JavaScript-Practico
+# Ejercicios de JavaScript
+
+## Figuras geométricas
+- Definir formulas
+- Implementar fórmulas en JavaScript
+- Crear funciones
+- Integrar JavaScrip con HTML
---
+++ figuras.js
@@ -0,0 +1,58 @@
+// Código del cuadrado
+console.group("Cuadrados");
+
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perimetro del cuadrado es: ${perimetroCuadrado}cm`)
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`El área del cuadrado es: ${areaCuadrado}cm^2`)
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log(`Los lados del triángulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm, ${baseTriangulo}cm`);
+console.log(`La altura del triángulo mide: ${alturaTriangulo}cm`);
+
+const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+baseTriangulo;
+console.log(`El perimetro del triángulo es: ${perimetroTriangulo}cm`);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log(`El area del triángulo mide: ${areaTriangulo}cm^2`);
+
+console.groupEnd();
+
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log(`El radio del círculo es: ${radioCirculo}cm`)
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log(`El diámetro del círculo es: ${diametroCirculo}cm`)
+
+// PI
+const  PI = Math.PI;
+console.log(`PI vale: ${PI}`)
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log(`El perimetro del círculo es: ${perimetroCirculo}cm`)
+
+// Área
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log(`El área del círculo es: ${areaCirculo}cm^2`)
+
+console.groupEnd();
